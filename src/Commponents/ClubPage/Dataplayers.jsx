34a12f6dde92234c577cdb7f0222dbe66281b339
Ezeopii <110064583+Ezeopii@users.noreply.github.com>
2023-11-10 18:03:52 -0300

    v5.5

--- src/Commponents/ClubPage/Dataplayers.jsx
+++ src/Commponents/ClubPage/Dataplayers.jsx
@@ -2,13 +2,19 @@ const Players = [
     { name: 'Juan douglas',
      positionJug: 'DFC', 
      league: 'Regional Rosarina',
-     photo: 'https://static.wikia.nocookie.net/liga-mx/images/b/bd/LuisArce.jpg/revision/latest/top-crop/width/200/height/150?cb=20230605055254&path-prefix=es'
+     photo: 'http://3.bp.blogspot.com/-DchCy20xPR0/T2NzSqRDfII/AAAAAAAAC8A/ZEPfMwwQdOE/s1600/abadie.jpg'
     },
     {
-        name: 'Juan douglas',
-        positionJug: 'DFC', 
+        name: 'Juan Sanchez',
+        positionJug: 'DC', 
         league: 'Regional Rosarina' ,
-        photo: 'http://3.bp.blogspot.com/-DchCy20xPR0/T2NzSqRDfII/AAAAAAAAC8A/ZEPfMwwQdOE/s1600/abadie.jpg'
+        photo: 'https://www.shutterstock.com/shutterstock/photos/2227618385/display_1500/stock-photo-professional-soccer-player-in-morocco-national-team-red-and-green-jersey-with-ball-in-hands-staring-2227618385.jpg'
+    },
+    {
+      name: 'Pablo Silva',
+        positionJug: 'MC', 
+        league: 'Regional Rosarina' ,
+        photo: 'https://i.pinimg.com/originals/43/cc/6b/43cc6b42cd03aa491ea7860a506ea8cf.jpg'
     }
     
   ];
